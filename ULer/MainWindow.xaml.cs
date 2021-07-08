4743bba861516797a0954eec06257e0b34d39d72
melancn <cc@cnuer.cn>
2021-07-08 15:02:21 +0800

    新增驼峰

--- ULer/MainWindow.xaml.cs
+++ ULer/MainWindow.xaml.cs
@@ -34,5 +34,62 @@ namespace ULer
         {
             textBox.Text = textBox.Text.ToLower();
         }
+
+        private void CamelCaseF_Click(object sender, RoutedEventArgs e)
+        {
+            string n = "";
+            string[] r = textBox.Text.Split('_');
+            foreach (string i in r)
+            {
+                n += i.Substring(0, 1).ToUpper() + i.Substring(1).ToLower();
+            }
+
+            textBox.Text = n;
+        }
+
+        private void CamelCase_Click(object sender, RoutedEventArgs e)
+        {
+            short c = 0;
+            string n = "";
+            string[] r = textBox.Text.Split('_');
+            foreach (string i in r)
+            {
+                if (c == 0)
+                {
+                    n += i.ToString().ToLower();
+                }
+                else
+                {
+                    n += i.Substring(0, 1).ToUpper() + i.Substring(1).ToLower();
+                }
+                c++;
+            }
+
+            textBox.Text = n;
+        }
+
+        private void UnderScoreCase_Click(object sender, RoutedEventArgs e)
+        {
+            Boolean nounder = false;
+            string n = "";
+            string t = textBox.Text;
+            for (int counter = 0; counter < t.Length; counter++)
+            {
+                char i = t[counter];
+                // 大写字母
+                nounder = i >= 65 && i <= 90 && counter > 0;
+                if (nounder)
+                { 
+
+                    n += '_';
+                    n += i.ToString().ToLower();
+                }
+                else
+                {
+                    n += i.ToString().ToLower();
+                }
+            }
+            textBox.Text = n;
+        }
     }
 }
